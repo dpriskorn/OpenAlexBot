e47d1f17b980ec40e60ce8c1e89a77269374ac25
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-13 10:41:06 +0100

    Sort enum

--- openalexbot/enums.py
+++ openalexbot/enums.py
@@ -2,20 +2,20 @@ from enum import Enum
 
 
 class Property(Enum):
-    MAIN_SUBJECT = "P921"
-    STATED_AS = "P1932"
-    TITLE = "P1476"
+    CITES_WORK = "P2860"
     DOI = "P356"
     INSTANCE_OF = "P31"
-    OPENALEX_ID = ""
-    PMID = "P698"
-    CITES_WORK = "P2860"
     ISSUE = "P433"
-    PAGES = "P304"
-    VOLUME = "P478"
-    PUBLISHED_IN = "P1433"
-    PUBLICATION_DATE = "P577"
     LANGUAGE_OF_WORK = "P407"
+    MAIN_SUBJECT = "P921"
+    OPENALEX_ID = ""
+    PAGES = "P304"
+    PMID = "P698"
+    PUBLICATION_DATE = "P577"
+    PUBLISHED_IN = "P1433"
+    STATED_AS = "P1932"
+    TITLE = "P1476"
+    VOLUME = "P478"
 
 
 class StatedIn(Enum):
