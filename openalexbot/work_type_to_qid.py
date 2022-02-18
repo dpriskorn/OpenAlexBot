4b2cb5bf7d79fef763d92e3efc881decab02ed7c
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-18 10:41:18 +0100

    Support book chapter

--- openalexbot/work_type_to_qid.py
+++ openalexbot/work_type_to_qid.py
@@ -10,7 +10,9 @@ class WorkTypeToQid(BaseModel):
             return "Q13442814"
         elif self.work.type == WorkType.JOURNAL_ARTICLE:
             return "Q13442814"
+        elif self.work.type == WorkType.BOOK_CHAPTER:
+            return "Q21481766"
         else:
             raise ValueError(f"{self.work.type} is not "
                              f"supported, report an issue here "
-                             f"https://github.com/dpriskorn/OpenAlexAPI/issues.")
+                             f"https://github.com/dpriskorn/OpenAlexBot/issues.")
